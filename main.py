b674d5cc056a08fdcb02c4ea1b129247bc5342e5
jease0502 <jease0502@gmail.com>
2021-12-09 23:23:45 +0800

    Add

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ from threading import Thread
 import cv2
 import time
 
-
 class VideoWriter(object):
-    def __init__(self, video_file_name, src=0):
+    def __init__(self, video_file_name : str, src : int , fps : int):
         """
-        Create a VideoCapture object
-        if using webcams, else just use src as it is.
+l        to record video and show video
         """
         self.src = src
-        self.video_file_name = video_file_name + '.avi'
         self.capture = cv2.VideoCapture(self.src, cv2.CAP_DSHOW)
 
         # Default resolutions of the frame are obtained (system dependent)
@@ -20,7 +17,7 @@ class VideoWriter(object):
         # Set up codec and output video settings
         self.codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
         self.output_video = cv2.VideoWriter(
-            self.video_file_name, self.codec, 120, (self.frame_width, self.frame_height))
+            video_file_name + '.avi', self.codec, fps, (self.frame_width, self.frame_height))
 
         # Start the thread to read frames from the video stream
         self.thread = Thread(target=self.update, args=())
@@ -29,7 +26,7 @@ class VideoWriter(object):
 
         # Start another thread to show/save frames
         self.start_recording()
-        print('initialized {}'.format(self.video_file_name[:-4]))
+        print('initialized {}'.format(video_file_name))
 
     def update(self):
         """
@@ -79,7 +76,7 @@ class VideoWriter(object):
 if __name__ == '__main__':
     video_writer_widget = list()
     for i in range(int(input("pls input how many camera you are: "))):
-        video_writer_widget.append(VideoWriter('Camera' + str(i), i))
+        video_writer_widget.append(VideoWriter('Camera' + str(i), i , 15))
 
     while True:
         time.sleep(0.000001)
