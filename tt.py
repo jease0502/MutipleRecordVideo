ef6c53bcf3c70de3b77ab1256b26d68874b379a5
jease0502 <jease0502@gmail.com>
2021-12-16 00:29:24 +0800

    Add motifty exposure

--- tt.py
+++ tt.py
@@ -2,8 +2,16 @@ import os
 import cv2
 import gc
 from multiprocessing import Process, Manager
+import time
+from tkinter import *
+from PIL import Image,ImageTk
 
-def write(stack, cam, top: int) -> None:
+class VideoRecoder(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+def write(queue, cam, top: int) -> None:
     """
     :param cam: 攝影機編號
     :param stack: Manager.Queue()
@@ -13,35 +21,40 @@ def write(stack, cam, top: int) -> None:
     print('Process to write: %s' % os.getpid())
     cap = cv2.VideoCapture(cam,cv2.CAP_DSHOW)
     cap.set(cv2.CAP_PROP_FPS, 15)
-
+    # print(cap.get(cv2.CAP_PROP_EX))
     while True:
         ret, img = cap.read()
         if not ret:
             continue
-        stack.put(img)
+        exposure = cap.get(cv2.CAP_PROP_EXPOSURE)
+        cap.set(cv2.CAP_PROP_EXPOSURE, -5 )
+        print(f"exposure {cam} : ",exposure)
+        queue.put(img)
 
-def save(stack, cam_id) -> None:
+def save(queue, cam_id) -> None:
     print('Process to save: %s' % os.getpid())
     codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-    output_video = cv2.VideoWriter(
-                f"{cam_id}" + '.avi', codec, 30, (640, 480))
+
+    output_video = cv2.VideoWriter("video//" + str(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())) + f"{cam_id}" + '.avi', codec, 30, (640, 480))
     while True:
-        frame = stack.get(True) # blocking operation
+        frame = queue.get(True) # blocking operation
         output_video.write(frame)
         cv2.imshow(f"cam {cam_id} img", frame)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             output_video.release()
             break
 
 if __name__ == '__main__':
-
+    cam1 = 1
+    cam2 = 2
     q = Manager().Queue()
     q1 = Manager().Queue()
-    pw = Process(target=write, args=(q, 1, 100))
-    ps = Process(target=save, args=(q, 1))
-    pw1 = Process(target=write, args=(q1, 0, 100))
-    ps1 = Process(target=save, args=(q1, 0))
+    pw = Process(target=write, args=(q, cam1, 100))
+    ps = Process(target=save, args=(q, cam1))
+    pw1 = Process(target=write, args=(q1, cam2, 100))
+    ps1 = Process(target=save, args=(q1, cam2))
     
     pw.start()
     pw1.start()
